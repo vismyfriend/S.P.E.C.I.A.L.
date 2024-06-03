ab880ff6f29d969eee9af552e49b7c4e0e7278cc
Vismyfriend <vismyfriend@yahoo.com>
2024-06-03 15:57:47 +0300

    roles1 set added

--- scripts/utils/questions.js
+++ scripts/utils/questions.js
@@ -435,6 +435,34 @@ const allQuestionsOneDeck = {
                 
                 },
     ],
+    roles1: [
+        {
+            id:1,
+            eng:"Карточка №1 вЭлэри My name is Valery. My pets are chickens. I like chickens a lot. I love beer very much. I am crazy about beer. I collect beer bottles.",
+            ru:"pets /пЭтс/ питомцы  chickens /чикенс/ куры a lot /э лот/ оч.сильно beer /биир/ пивасик bottles /бОтэлз/ бутылки"
+            },
+            {
+            id:2,
+            eng:"Карточка №2 пИги My name is Pigi. My pets are pigs. I like pigs a lot. I love martini very much. I am crazy about martini. I collect martini bottles.",
+            ru:"pets /пЭтс/ питомцы  pigs /пигз/ свинюшки a lot /э лот/ оч.сильно bottles /бОтэлз/ бутылки"
+            },
+            {
+            id:3,
+            eng:"Карточка №3 Джонни My name is Johny. My pets are frogs. I like frogs a lot. I love soups very much. I am crazy about soups. I collect frog legs.",
+            ru:"pets /пЭтс/ питомцы  frogs /фрогз/ лягушки a lot /э лот/ оч.сильно soups /супс/ супы frog legs легушачьи лапки"
+            },
+            {
+            id:4,
+            eng:"Карточка №4 Вивальди My name is Vivaldi. My pets are birds бёрдз I like birds a lot. I love dranikis very much. I am crazy about dranikis. I collect bird photos.",
+            ru:"pets /пЭтс/ питомцы  birds /бёрдз/ птички a lot /э лот/ оч.сильно photos /фОтос/ фотки dranikis - некоторых слов в английском нет, драники так и будет"
+            },
+            {
+            id:5,
+            eng:"Карточка №5 мЭнни My name is Manny. My pets are donkeys. I like donkeys a lot. I love sweets very much. I am crazy about sweets. I collect magnets мЭгнэтс.",
+            ru:"pets /пЭтс/ питомцы  donkeys /дАнкиз/ ослики a lot /э лот/ оч.сильно sweets /свитс/ конфеты magnets /мЭгнэтс/ магниты"
+            },
+         
+    ],
     devmode1: [
         {
             id:1,
