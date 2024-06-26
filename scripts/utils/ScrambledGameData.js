2a05c1ad3d31ba4fe07e3ae1d64c4d1285285200
Vismyfriend <vismyfriend@yahoo.com>
2024-06-26 17:59:57 +0300

    professions 1 set added

--- scripts/utils/ScrambledGameData.js
+++ scripts/utils/ScrambledGameData.js
@@ -20,6 +20,98 @@ const allWordsForScrambledGame = {
             ru:"третий"
             },
     ],
+    professions1: [
+        {
+            id:1,
+            eng:"teachers",
+            ru:"учителя",
+            hint:" / тичерз /"
+            },
+            {
+            id:2,
+            eng:"students",
+            ru:"учащиеся",
+            hint:" / стьюдэнтся /"
+            },
+            {
+            id:3,
+            eng:"readers",
+            ru:"читатели",
+            hint:" / ридэрз /"
+            },
+            {
+            id:4,
+            eng:"writers",
+            ru:"писатели",
+            hint:" / райтэрз /"
+            },
+            {
+            id:5,
+            eng:"dancers",
+            ru:"танцоры",
+            hint:" / дэнсэрз /"
+            },
+            {
+            id:6,
+            eng:"killers",
+            ru:"убийцы",
+            hint:" / килэрз /"
+            },
+            {
+            id:7,
+            eng:"robbers",
+            ru:"грабители",
+            hint:" / роббэрз /"
+            },
+            {
+            id:8,
+            eng:"drivers",
+            ru:"водители",
+            hint:" / дрАйвэрз /"
+            },
+            {
+            id:9,
+            eng:"singers",
+            ru:"певцы",
+            hint:" / сИнгэрз /"
+            },
+            {
+            id:10,
+            eng:"builders",
+            ru:"строители",
+            hint:" / бИлдэрз /"
+            },
+            {
+            id:11,
+            eng:"sportsmen",
+            ru:"спортсмены",
+            hint:" / спОртсмен /"
+            },
+            {
+            id:12,
+            eng:"runners",
+            ru:"бегуны",
+            hint:" / раннэрз /"
+            },
+            {
+            id:13,
+            eng:"jumpers",
+            ru:"прыгуны",
+            hint:" / джАмпэрз /"
+            },
+            {
+            id:14,
+            eng:"swimmers",
+            ru:"пловцы (плаватели)",
+            hint:" / свиммэрз /"
+            },
+            {
+            id:15,
+            eng:"managers",
+            ru:"менеджеры",
+            hint:" / мЭнэджэрз /"
+            },
+    ],
     scrambledWords1: [
         {
             id:1,
